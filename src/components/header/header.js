483c5be3e82d3278247bd0dcd8274adec5304fe7
v.Panfilov <pupazoed@gmail.com>
2020-05-08 12:43:31 +0300

    first

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,89 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import React from "react"
 import styles from "./header.module.css"
+// import classnames from "classnames"
+import rupor from "../../images/bg.svg"
+
 
 export const Header = ({ siteTitle }) => (
   <header>
-      <h1 className={styles.test}>
-        <Link to="/">
-          {siteTitle}
-        </Link>
-      </h1>
+    {/*<div className={styles.mynavbar}>Tl;jfl;dk;dslfkg</div>*/}
+
+    {/*<meta name="viewport" content="width=device-width, initial-scale=1">*/}
+    <img src={rupor} alt={"rupor"} className={styles.ruporHeader}/>
+
+    {/*<div className={classnames(styles.navContainer, styles.mynavbar)}>*/}
+    {/*</div>*/}
+
+      {/*<div className={styles.flyout} id="flyout-example">*/}
+    {/*/!*<div className="flyout-debug" id="flyout-debug"></div>*!/*/}
+    {/*<div className={styles.flyoutButtons}*/}
+    {/*<a className={styles.logoLink} href = "https://dex-it.ru">*/}
+    {/*<img className={styles.logo} src='../../images/logo.svg'/>*/}
+    {/*<span className={styles.bottomLogo}> iT компания #1</span>*/}
+    {/*<!--<div class="logo"></div>-->*/}
+    {/*</a>*/}
+    {/*<ul id="myTopnav" className="nav navbar-nav navbar-right navbar">*/}
+    {/*<div className="cont-resp">*/}
+    {/*<a href="#">*/}
+    {/*<div className={styles.logo}></div>*/}
+    {/*</a>*/}
+    {/*<a href="#">*/}
+    {/*<img src="../../images/close.svg" alt="close"/>*/}
+    {/*</a>*/}
+    {/*</div>*/}
+    {/*/!*<a onClick="closeBurger()" className="link-img level_1" href="#about">НАШИ ПЛЮШКИ</a>*!/*/}
+    {/*/!*<a onClick="closeBurger()" className="link-img level_1" href="#rewards">МЫ В РЕЙТИНГАХ</a>*!/*/}
+    {/*/!*<a onClick="closeBurger()" className="link-img level_1" href="#events">МЕРОПРИЯТИЯ</a>*!/*/}
+    {/*/!*<a onClick="closeBurger()" className="link-img level_1" href="#requires">КОНТАКТЫ</a>*!/*/}
+    {/*/!*<a href="javascript:void(0);" className="icon" onClick="Burger()">*!/*/}
+    {/*/!*<img className="burgerBlue" src="../../images/burger_icon_blue.svg" alt="burger">*!/*/}
+    {/*/!*<img className="burgerWhite" src="../../images/burger_icon.svg" alt="burger">*!/*/}
+    {/*/!*</a>*!/*/}
+    {/*</ul>*/}
+    {/*</div>*/}
+    {/*</div>*/}
+    {/*</div>*/}
+
+    {/*<div className="container-fluid">*/}
+    {/*<div className="send">*/}
+    {/*<span className="need">*/}
+    {/*Нам нужны*/}
+    {/*</span>*/}
+
+    {/*<!-- Carousel container -->*/}
+    {/*<div className="carousel slide" data-ride="carousel" data-interval="2000">*/}
+
+    {/*<!-- Content -->*/}
+    {/*<div className="carousel-inner" role="listbox">*/}
+    {/*<!-- Slide 1 -->*/}
+    {/*<div className="position item active">*/}
+    {/*Mobile*/}
+    {/*</div>*/}
+    {/*<!-- Slide 2 -->*/}
+    {/*<div className="position item">*/}
+    {/*Back-end*/}
+    {/*</div>*/}
+    {/*<!-- Slide 3 -->*/}
+    {/*<div className="position item">*/}
+    {/*Front-end*/}
+    {/*</div>*/}
+    {/*<!-- Slide 4 -->*/}
+    {/*<div className="position item">*/}
+    {/*ML*/}
+    {/*</div>*/}
+    {/*<a href="#" className="btn btn-dex btn-extra-lg" data-toggle="modal" data-target="#sendResume">Напишите*/}
+    {/*нам</a>*/}
+    {/*</div>*/}
+    {/*</div>*/}
+    {/*<span className="our-clients">Наши клиенты</span>*/}
+    {/*<div>*/}
+    {/*<img className="client-icon" src="../../images/hugges.svg">*/}
+    {/*<img className="client-icon" src="../../images/nokia.svg">*/}
+    {/*<img className="client-icon" src="../../images/vtb.svg">*/}
+    {/*<img className="client-icon" src="../../images/sber.svg">*/}
+    {/*</div>*/}
+    {/*</div>*/}
+    {/*</div>*/}
   </header>
 )
